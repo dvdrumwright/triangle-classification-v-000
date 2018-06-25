2c67afd41079f865da22430f4887e52768a21c3d
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-25 23:28:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,12 +19,12 @@ class Triangle
        :isosceles
  elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
         :scalene
-end 
-end 
+
+   end 
+  end 
 end
 
 
 class TriangleError < StandardError
-  # triangle error code
 end
   
