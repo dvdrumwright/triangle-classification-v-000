144ab16668c03685c336f8433a6cdfddeff93031
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-25 22:47:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,9 @@ def kind
   if @side_1 && @side_2 && @side_3 == 0 
     raise TriangleError
   elsif @side_1 + @side_2 <= @side_3 || 
-        @side_1 + 
+        @side_1 + @side_3 <= @side_2 || 
+        @side_3 + @side_2 <= @side_1 
+        
 
 
 
