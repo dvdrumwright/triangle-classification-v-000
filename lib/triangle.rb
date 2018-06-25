a4904218f21b66fc2cb943d390b74f751c1e3987
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-25 23:19:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,9 +15,7 @@ class Triangle
   end 
   if  @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
-  elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
-        :isosceles
-
+  elsif 
 end 
 end 
 
