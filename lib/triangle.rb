43f0956423ce2ad78b22a3fc2312ef1daf838e13
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-25 23:01:50 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
   end 
   if  @side_1 + @side_2 + @side_3 = 3 
     return :equilateral 
-    @side_1 + @side_2 + @side_3 = 3 
+   elsif @side_1 + @side_2 + @side_3 = 3 
     return :scalene 
     elsif @side_1 + @side_2 + @side_3 = 2
     return :isosceles 
