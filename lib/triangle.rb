00ce96b553e5fc6e837a51279ea15e9de0228f69
Daniel  Drumwright <danieldrumwright@gmail.com>
2018-06-25 23:18:48 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,8 @@ class Triangle
   end 
   if  @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
-  elsif @side_1
+  elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+  
 
 end 
 end 
